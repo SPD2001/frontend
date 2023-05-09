2b4254a86d71aae7f272956f98a855ba5e5cf25a
sindhupd-2001 <98105619+sindhupd-2001@users.noreply.github.com>
2023-05-09 18:42:36 +0530

    added requirment

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  formdata = {email:"",password:""};
+  submit=false;
+  loading=false;
+  errorMessage="";
+  constructor(private auth:AuthService) { }
+
+  ngOnInit(): void {
+    this.auth.canAuthenticate();
+  }
+
+  onSubmit(){
+    this.loading=true;
+    this.auth.login(this.formdata.email,this.formdata.password)
+    .subscribe({
+        next:(data: { idToken: string; })=>{
+            this.auth.storeToken(data.idToken);
+            console.log('logged user token is '+data.idToken);
+            this.auth.canAuthenticate();
+        },
+        error:(data: { error: { error: { message: string; }; }; })=>{
+            if (data.error.error.message=="INVALID_PASSWORD" || data.error.error.message=="INVALID_EMAIL") {
+                this.errorMessage = "Invalid Credentials!";
+            } else{
+                this.errorMessage = "Unknown error when logging into this account!";
+            }
+        }
+    }).add(()=>{
+        this.loading =false;
+        console.log('login process completed!');
+
+    })
+  }
 
 }
